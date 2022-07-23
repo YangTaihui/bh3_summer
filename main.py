2aeaed6d83b9ddd7da0b871144d326267dde16d7
YangTaihui <thuiyang@126.com>
2022-07-23 21:08:55 +0800

    feat: add 7.24

--- main.py
+++ main.py
@@ -8,65 +8,57 @@ from peoples import *
 
 # fight(V2V, KeSiMo)
 # fights(V2V(), KeSiMo(), 10000)
-# fights(KeSiMo(), V2V(), 10000)
 
 # fight(KaiWen, KeSiMo)
 # fights(KaiWen(), KeSiMo(), 10000)
-# fights(KeSiMo(), KaiWen(), 10000)
 
 # 7.19
 # fight(ABoNiYa, GeLeiXiu)
 # fights(ABoNiYa, GeLeiXiu, 10000)
-# fights(GeLeiXiu, ABoNiYa, 10000)
 
 # fight(Cat, GeLeiXiu)
 # fights(Cat, GeLeiXiu, 10000)
-# fights(GeLeiXiu, Cat, 10000)
 
 # fight(Cat, ABoNiYa)
 # fights(Cat, ABoNiYa, 10000)
-# fights(ABoNiYa, Cat, 10000)
 
 # 7.20
 # fight(AiLiXiYa, MeiBiWuSi)
 # fights(AiLiXiYa, MeiBiWuSi, 10000)
-# fights(MeiBiWuSi, AiLiXiYa, 10000)
 
 # fight(Hua, MeiBiWuSi)
 # fights(Hua, MeiBiWuSi, 10000)
-# fights(MeiBiWuSi, Hua, 10000)
 
 # fight(Hua, AiLiXiYa)
 # fights(Hua, AiLiXiYa, 10000)
-# fights(AiLiXiYa, Hua, 10000)
 
 # 7.21
 # fight(YiDian, QianJie)
 # fights(YiDian, QianJie, 10000)
-# fights(QianJie, YiDian, 10000)
 
 # fight(QianJie, Ying)
 # fights(QianJie, Ying, 10000)
-# fights(Ying, QianJie, 10000)
 
 # fight(YiDian, Ying)
 # fights(YiDian, Ying, 10000)
-# fights(Ying, YiDian, 10000)
 
 # 7.22 8进4
 # fight(KaiWen, AiLiXiYa)
 # fights(KaiWen, AiLiXiYa, 10000)
-# fights(AiLiXiYa, KaiWen, 10000)
 
 # fight(YiDian, GeLeiXiu)
 # fights(YiDian, GeLeiXiu, 10000)
-# fights(GeLeiXiu, YiDian, 10000)
 
 # 7.23 8进4
 # fight(Hua, V2V)
 # fights(Hua, V2V, 10000)
-# fights(V2V, Hua, 10000)
 
 # fight(Ying, ABoNiYa)
 # fights(Ying, ABoNiYa, 10000)
-# fights(ABoNiYa, Ying, 10000)
+
+# 7.24 半决赛
+# fight(AiLiXiYa, GeLeiXiu)
+# fights(AiLiXiYa, GeLeiXiu, 10000)
+
+# fight(Hua, ABoNiYa)
+# fights(Hua, ABoNiYa, 10000)
